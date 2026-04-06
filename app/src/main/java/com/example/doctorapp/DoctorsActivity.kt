82b26eb705367fb91227a6e0819ff28ca382c00e
Andrey <email@example.com>
2026-04-06 10:18:38 +0700

    ;sdkfs

--- app/src/main/java/com/example/doctorapp/DoctorsActivity.kt
+++ app/src/main/java/com/example/doctorapp/DoctorsActivity.kt
@@ -1,35 +1,25 @@
 package com.example.doctorapp
 
-import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 
-class MainActivity : AppCompatActivity() {
+class DoctorsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_doctors)
 
         // window.statusBarColor = Color.parseColor("#40000000")
         WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightNavigationBars = true
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, 0, systemBars.right, 0)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
             insets
-        }
-
-        // Обработчик нажатия кнопки "Найти врача"
-        val findDoctorButton = findViewById<Button>(R.id.findDoctor)
-        findDoctorButton.setOnClickListener {
-            val intent = Intent(this, DoctorsActivity::class.java)
-            startActivity(intent)
         }
     }
 }
